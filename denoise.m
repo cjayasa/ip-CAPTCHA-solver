408c7de5ea012795a4e7d4ccc81395d33fddf2aa
Chathura Jayasanka <kalugala-muhandiramge.chathura-jayasanka.8404@student.uu.se>
2023-01-01 11:02:23 +0100

    preprocess added

--- denoise.m
+++ denoise.m
@@ -1,4 +1,25 @@
 function processed_image = denoise(I)
 
-    processed_image = rgb2gray(I);
+    % Apply grayscale
+    I1 = rgb2gray(I);
+
+    % Apply median filter
+    I2 = medfilt2(I1,[7 7]);
+
+    % Apply gaussian filter
+    I3 = imgaussfilt(I2,2);
+
+    % Binarize image
+    I4 = ~im2bw(I3,graythresh(I3));
+    %I4 = imbinarize(I3,graythresh(I3));
+
+    % Apply erode
+    I5 = imerode(I4, strel('disk',5));
+
+    % Remove small regions less than 500 pixels
+    I6 = bwareaopen(I5, 500);
+
+    % Apply dilate
+    I7 = imdilate(I6, strel('square',4));
+    processed_image = I7;
 end
